8819bff719be6ee4e63ef5347415dc141425d729
LucasLeopolski <126730157+LucasLeopolski@users.noreply.github.com>
2023-05-17 21:56:21 -0300

    heranca

---
+++ TrabalhandoHeranca/src/main/java/com/mycompany/trabalhandoheranca/Funcionario.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.trabalhandoheranca;
+
+/**
+ *
+ * @author lucas
+ */
+public class Funcionario {
+    public String nome; 
+    
+    public Funcionario(String aNome){
+    this.nome= aNome; 
+    }
+    
+}
---
+++ TrabalhandoHeranca/src/main/java/com/mycompany/trabalhandoheranca/TrabalhandoHeranca.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabalhandoheranca;
+
+/**
+ *
+ * @author lucas
+ */
+public class TrabalhandoHeranca {
+
+    public static void main(String[] args) {
+       Gerente gerente1 = new Gerente("AAAAa"); 
+       //gerente1.nome = "Lucas";
+       
+        System.out.println(gerente1.nome);
+       
+       
+        
+    }
+}
