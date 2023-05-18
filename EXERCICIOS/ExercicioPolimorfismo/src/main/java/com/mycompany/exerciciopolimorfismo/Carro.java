e6fe8f981c18cec585d570d2693ac06eb042906a
LucasLeopolski <126730157+LucasLeopolski@users.noreply.github.com>
2023-05-17 22:23:00 -0300

    Exercicio

---
+++ EXERCICIOS/ExercicioPolimorfismo/src/main/java/com/mycompany/exerciciopolimorfismo/Carro.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.exerciciopolimorfismo;
+
+/**
+ *
+ * @author lucas
+ */
+public class Carro extends Veiculo {
+    
+    
+    @Override
+    public  void mover(){
+        System.out.println("O carro está se movendo!");
+    }
+    
+}
---
+++ EXERCICIOS/ExercicioPolimorfismo/src/main/java/com/mycompany/exerciciopolimorfismo/ExercicioPolimorfismo.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exerciciopolimorfismo;
+
+import java.util.ArrayList;
+
+/**
+ *ver
+ * @author lucas
+ */
+public class ExercicioPolimorfismo {
+
+    public static void main(String[] args) {
+        
+        Veiculo[] veiculos = new Veiculo[2];
+        veiculos[0] = new Carro(); 
+        veiculos[1] = new Aviao();
+        
+        for(Veiculo veiculo: veiculos){
+        
+        veiculo.mover();
+        }
+    }
+}
